7532047cf70460824b84f7399dd7145439163fa9
pavanchavan-web <your-email-used-in-github>
2026-01-08 12:45:11 +0530

    Fix title and Disc

--- frontend/src/pages/ConvertToSvg.jsx
+++ frontend/src/pages/ConvertToSvg.jsx
@@ -4,8 +4,8 @@ export default function ConvertToSvg() {
   return (
     <ImageConvert
       targetFormat="svg"
-      title="Convert to SVG"
-      desc="Convert WEBP, JPG, JPEG, GIF, TIFF, AVIF, BMP, JPG or RAW images format to SVG online in bulk with zero quality loss."
+      title="SVG to Others"
+      desc="Convert SVG format to WEBP, JPG, JPEG, GIF, TIFF, AVIF, PNG online in bulk with zero quality loss."
       maxText="Max 20 images per upload"
     />
   );
