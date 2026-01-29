21a1ca0f14c81795e75f30fc45e62a4e557e5255
pavanchavan-web <your-email-used-in-github>
2026-01-29 17:09:35 +0530

    blogs

--- frontend/src/lib/sanity.js
+++ frontend/src/lib/sanity.js
@@ -2,7 +2,7 @@ import { createClient } from "@sanity/client";
 
 export const sanityClient = createClient({
   projectId: "oo371u7j",
-  dataset: "production",
+  dataset: "blogs",
   apiVersion: "2024-01-01",
   useCdn: true
 });
